fb72073944f2f3c437fc4be3ed3914d6415cad29
Simon <simon@simonhamilton.ca>
2018-01-22 23:40:06 +0000

    ability to pull out id and avatar_url

--- download_avatars.js
+++ download_avatars.js
@@ -10,17 +10,32 @@ function getRepoContributors(repoOwner, repoName, cb) {
       'User-Agent': 'request',
       'Authorization': 'token ' + secrets.GITHUB_TOKEN
     }
+
   };
+  //request(options, function(err, res, body) {
   request(options, function(err, res, body) {
-    cb(err, body)
+
+    if (err) console.log('error has occured');
+    // parse the JSON string into an object
+    let parsed = JSON.parse(body);
+    //console.log("B", parsed);
+    console.log("C", parsed[0].login);
+    console.log("D", parsed[0].avatar_url);
+
+// pass this object (an array of contributor objects) to the cb function
+// modify the callback function to iterate over results and
+// console.log the value for each avatar_url
+    //cb(err, result);
+
+
   });
 }
 
 
 
 getRepoContributors("jquery", "jquery", function(err, result) {
-  console.log("errors:", err);
-  console.log("result:", result);
+  // console.log("errors:", err);
+  // console.log("result:", result);
 })
 
 // reminder: cb or callbacks in node expect their first argument
