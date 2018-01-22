1090dd907c396688a40fc9b1da97c8c8b73ba8bf
Simon <simon@simonhamilton.ca>
2018-01-22 21:56:40 +0000

    started the getRepoContributors function

--- download_avatars.js
+++ download_avatars.js
@@ -1,2 +1,14 @@
 var request = require('request');
 console.log('Welcome to the GitHub Avatar Downloader!');
+
+function getRepoContributors(repoOwner, repoName, cb) {
+  // ...
+}
+
+getRepoContributors("jquery", "jquery", function(err, result) {
+  console.log("errors:", err);
+  console.log("result:", result);
+})
+
+// reminder: cb or callbacks in node expect their first argument
+//to be a placeholder for any errors
