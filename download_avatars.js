06d21dc81e78b921822c5801bdfa3d35d28b2c05
Simon <simon@simonhamilton.ca>
2018-01-23 00:19:41 +0000

    link download_image and create a new file to hold downloaded images named by login_id

--- download_avatars.js
+++ download_avatars.js
@@ -1,5 +1,7 @@
 var request = require('request');
 var secrets = require('./secrets');
+var fs = require('fs');
+
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
@@ -12,15 +14,18 @@ function getRepoContributors(repoOwner, repoName, cb) {
     }
 
   };
-  //request(options, function(err, res, body) {
+
   request(options, function(err, res, body) {
 
     if (err) console.log('error has occured');
     // parse the JSON string into an object
     let parsed = JSON.parse(body);
+    for (i = 0; i < parsed.length; i++) {
+      downloadImageByURL(parsed[i].avatar_url, "avatars/" + parsed[i].login + ".jpeg");
+    }
     //console.log("B", parsed);
-    console.log("C", parsed[0].login);
-    console.log("D", parsed[0].avatar_url);
+    // console.log("C", parsed[0].login);
+    // console.log("D", parsed[0].avatar_url);
 
 // pass this object (an array of contributor objects) to the cb function
 // modify the callback function to iterate over results and
@@ -31,12 +36,26 @@ function getRepoContributors(repoOwner, repoName, cb) {
   });
 }
 
+function downloadImageByURL(url, filePath) {
+  //make a request to given url
+  request.get(url)
+    .on('error', function (err) {
+      console.log("we have an error");
+    })
+    .on('response', function(response) {
+      console.log('Response Status Code:', response.statusCode);
+    })
+    .pipe(fs.createWriteStream(filePath));
+  // save the resulting image to specified filepath
 
+}
 
-getRepoContributors("jquery", "jquery", function(err, result) {
+// downloadImageByURL("https://avatars2.githubusercontent.com/u/2741?v=3&s=466", "avatars/kvirani.jpg");
+
+getRepoContributors("jquery", "jquery")
   // console.log("errors:", err);
   // console.log("result:", result);
-})
+
 
 // reminder: cb or callbacks in node expect their first argument
 //to be a placeholder for any errors
